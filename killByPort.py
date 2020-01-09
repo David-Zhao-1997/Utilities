d313f8902f7b461487df4ab11fb086cdbc3c1eeb
qduzhl <550361254@qq.com>
2020-01-10 07:23:12 +0800

    first commit

---
+++ killByPort.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+import os
+import platform
+import subprocess
+import sys
+
+
+def killProcessWin(port):
+    p = subprocess.Popen("cmd.exe /c" + "netstat -aon|findstr \"" + str(port) + "\"", stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    curLine = p.stdout.readline()
+    while curLine != b'':
+        lineArr = curLine.split(" ".encode())
+        pid = int(lineArr[-1].replace(os.linesep.encode(), "".encode()))
+        try:
+            if pid != 0:
+                subprocess.Popen("cmd.exe /k taskkill /F /T /PID %i" % pid, shell=True)
+                print("pid : " + str(pid), "", "killed")
+            else:
+                print("nothing to kill")
+        except OSError:
+            pass
+        curLine = p.stdout.readline()
+    p.wait()
+
+
+def killProcessUnix(port):
+    p = subprocess.Popen("lsof -i:" + str(port)+" | grep \""+str(port)+"\"", stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT, shell=True)
+    curLine = p.stdout.readline()
+    while curLine != b'':
+        lineArr = curLine.split()
+        pid = lineArr[1].replace(os.linesep.encode(), "".encode())
+        result = os.system("kill -9 " + pid)
+        if result == 0:
+            print("pid : " +pid +" killed")
+        else:
+           pass
+        curLine = p.stdout.readline()
+    p.wait()
+
+
+def killByPlatform(port):
+    sysStr = platform.system()
+    if sysStr == "Windows":
+        killProcessWin(port)
+    elif sysStr == "Linux":
+        killProcessUnix(port)
+    elif sysStr == "Darwin":
+        killProcessUnix(port)
+    else:
+        print("Platform not supported yet!")
+
+
+killByPlatform(sys.argv[1])
